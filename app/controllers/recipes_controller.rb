e3eda2cddbe34a7f16c8ccb491a517c8e5a7c496
Ashour15 <mohamedashour15@hotmail.com>
2021-12-09 04:22:15 +0200

    Set the relation between the models, init jquery datatable and setup recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,15 @@
 class RecipesController < ApplicationController
   def index
+    ingredients_names = params[:ingredients_names].split(',')
+    ingredients_count = ingredients_names.length
+    @recipes = Recipe.where(ingredients_count: ingredients_count).
+      joins(:ingredients).
+      where(ingredients: {name: ingredients_names })
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @recipes }
+    end
   end
 end
+  
