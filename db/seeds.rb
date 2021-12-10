7c51cd2c2aae186ad8514d62314535e1f76f0861
Ashour15 <mohamedashour15@hotmail.com>
2021-12-10 22:24:57 +0200

    Add column quantity to recipe ingredients table and create seed file to import all recipe records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def create_recipe(recipe_hash)
+  recipe = Recipe.create(
+    name: recipe_hash['name'],
+    prep_time: recipe_hash['prep_time'],
+    rate: recipe_hash['rate'],
+    author_tip: recipe_hash['author_tip'],
+    budget: recipe_hash['budget']
+  )
+  puts "Created recipe #{recipe_hash['name']}"
+  return recipe
+end
+
+def create_ingredient(ingredient, recipe_id)
+  if check_first_character_is_number(ingredient)
+    ingredient_quantity = ingredient.split(' ', 2).first
+    ingredient_name = ingredient.split(' ', 2).last
+  else
+    ingredient_quantity = 1
+    ingredient_name = ingredient
+  end
+    ingredient = Ingredient.find_or_create_by(name: ingredient_name.downcase)
+    puts "Created ingredient #{ingredient_name}"
+    RecipeIngredient.create(recipe_id: recipe_id, ingredient_id: ingredient.id,
+        quantity: ingredient_quantity)
+    puts "Created RecipeIngredient #{ingredient_name} for Recipe #{recipe_id}"
+end
+
+def check_first_character_is_number(ingredient)
+  ingredient[0] >= '0' && ingredient[0] <= '9'
+end
+
+if Recipe.count == 0
+  file = File.read('./recipes.json')
+  data = file.split("\n")
+  data.each do |data_hash|
+    recipe_hash = JSON.parse(data_hash)
+    recipe = create_recipe(recipe_hash)
+    ingredients_list = recipe_hash['ingredients']
+    ingredients_list.each do |ingredient_string|
+      create_ingredient(ingredient_string, recipe.id)
+    end
+  end
+end
