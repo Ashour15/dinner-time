ecb4de6e2467289bf17c0b8612f1e66ebd251f72
Ashour15 <mohamedashour15@hotmail.com>
2021-12-10 22:26:26 +0200

    Modify get recipes query and include the recipe ingredients and remove unused files

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,10 @@
 class RecipesController < ApplicationController
   def index
     ingredients_names = params[:ingredients_names].split(',')
-    @recipes = Recipe.joins(:ingredients).
-      where(ingredients: {name: ingredients_names })
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @recipes }
-    end
+    ingredients_names.map!(&:downcase)
+     @recipes = Recipe.joins(:ingredients).
+      where(ingredients: { name: ingredients_names }).
+      preload(:ingredients)
   end
 end
   
